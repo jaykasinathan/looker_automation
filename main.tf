360acf1851a936d949f6cb0c16583047485a1b0f
jaykasinathan <jay.kasinathan@digitalturbine.com>
2023-09-06 09:38:03 -0500

    Updated Config

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file(var.credentials_file)
-  project     = var.project_id
-  region      = var.region
+  credentials = file(var.google_config.credentials_file)
+  project     = var.google_config.project_id
+  region      = var.google_config.region
 }
